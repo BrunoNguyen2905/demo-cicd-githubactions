7381b6caeea2533f7ec79b353deb14bb0ca7a9aa
BrunoNguyen2905 <nhannguyen290520@gmail.com>
2021-01-16 13:35:45 +0200

    add autodeploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Hello World hihihihihi.
+          Edit <code>src/App.js</code> and Hello World hihihihihi hahahahaha.
         </p>
         <a
           className="App-link"
